a78211a927c1fb4ab556a051cdc33c90c6561de2
mgalz <78116433+mgalz@users.noreply.github.com>
2021-04-20 18:11:24 -0400

    update mongodb to use my database: Cluster0

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-	mongoURI: "mongodb+srv://dev:Frosty@1Frosty@cluster0.hxzpp.mongodb.net/Cluster0?retryWrites=true&w=majority",
+	mongoURI: "mongodb+srv://mgalz94:May28947!@cluster0.nt1nz.mongodb.net/Cluster0?retryWrites=true&w=majority",
 	secretOrKey: "oa7wHOKBTr",
 };
